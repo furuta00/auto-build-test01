609cfde6a56190fcd165ecb0d0b9f81779642357
古田祐章 <furuta@sas-com.com>
2016-08-31 15:48:41 +0900

    new
    新規テスト01

---
+++ Test01.java
@@ -0,0 +1,18 @@
+/*Class:Test01
+  Date:2015/04/02
+  Comment:文字の表示 */
+  
+public class Test01{
+	
+	public static void main(String[] args){
+		
+		//自分の名前を表示
+		System.out.println("AAA");
+		
+		//誕生日を表示します
+		System.out.println("2016");
+		
+		//年齢を表示します
+		System.out.println("26歳");
+	}
+}
---
+++ Test02.java
@@ -0,0 +1,19 @@
+/*Class:Test02
+  Date:2015/04/02
+  Comment:数値の表示と計算 */
+  
+public class Test02{
+	
+	public static void main(String[] args){
+		
+		//数値の10を表示
+		System.out.println(10);
+		
+		//数値の5を表示
+		System.out.println(5);
+		
+		//10 + 5の足し算結果を表示
+		System.out.println(10 + 5);
+		
+	}
+}
